99e90644a408334a951efe8808ebe10f27548047
Ianduha13 <ianduhamelhayes@gmail.com>
2023-09-03 21:43:47 -0300

    Adding header welcome and popular section

--- components/home/welcome/Welcome.jsx
+++ components/home/welcome/Welcome.jsx
@@ -12,7 +12,10 @@ import { useRouter } from "expo-router"
 import styles from "./welcome.style"
 import { icons, SIZES } from "../../../constants"
 
+const jobTypes = ["Full-time", "Part-time", "Contractor"]
+
 const Welcome = () => {
+  const [activeJobType, setActiveJobType] = useState("Full-time")
   const router = useRouter()
 
   return (
@@ -30,6 +33,32 @@ const Welcome = () => {
             style={styles.searchInput}
           />
         </View>
+        <TouchableOpacity style={styles.searchBtn} onPress={() => {}}>
+          <Image
+            source={icons.search}
+            resizeMode="contain"
+            style={styles.searchBtnImage}
+          />
+        </TouchableOpacity>
+      </View>
+      <View style={styles.tabsContainer}>
+        <FlatList
+          horizontal
+          data={jobTypes}
+          keyExtractor={(item) => item}
+          contentContainerStyle={{ columnGap: SIZES.small }}
+          renderItem={({ item }) => (
+            <TouchableOpacity
+              style={styles.tab(activeJobType, item)}
+              onPress={() => {
+                setActiveJobType(item)
+                router.push(`/search/${item}`)
+              }}
+            >
+              <Text style={styles.tabText(activeJobType, item)}>{item}</Text>
+            </TouchableOpacity>
+          )}
+        />
       </View>
     </View>
   )
